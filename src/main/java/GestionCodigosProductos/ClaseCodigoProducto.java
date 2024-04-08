d0de2684fc018ce2734cc333e0f488f913bc8efb
Patrik <psirb@myuax.com>
2024-04-08 20:31:39 +0200

    metodo generaCodigo

--- src/main/java/GestionCodigosProductos/ClaseCodigoProducto.java
+++ src/main/java/GestionCodigosProductos/ClaseCodigoProducto.java
@@ -1,11 +1,19 @@
 package GestionCodigosProductos;
 
+import java.util.ArrayList;
+import java.util.IllegalFormatCodePointException;
+
 public class ClaseCodigoProducto {
 
+    public static ArrayList<String> productos = new ArrayList<>();
+    public static int contadorArticulos = 1; //Inicializamos el contador de articulos a 1
 
     public String GeneraCodigo(String descripcionProducto, String categoria){
-        String codigo = "";
+        if (descripcionProducto == null || descripcionProducto.isEmpty() || categoria == null || categoria.isEmpty()){
+            throw new IllegalArgumentException("La descripción del producto y la categoría no pueden estar vacías");
+        }
 
+        String codigo = descripcionProducto.trim().charAt(0) + "-" + categoria.toUpperCase().charAt(0) + "-" + contadorArticulos;
         return codigo;
     }
 }
