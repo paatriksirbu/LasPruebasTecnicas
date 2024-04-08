46fd1defed68b0984827197d775a2136b694a368
Patrik <psirb@myuax.com>
2024-04-08 20:59:30 +0200

    working main

--- src/main/java/GestionCodigosProductos/GestionProductos.java
+++ src/main/java/GestionCodigosProductos/GestionProductos.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class GestionProductos {
     public static void main(String[] args) {
-        System.out.println("Gestión de productos");
+        System.out.println("    --Gestión de productos--\n");
+        do{
+            Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
+            System.out.println("Introduce la descripción del producto:");
+            String descripcionProducto = sc.nextLine();
 
-        System.out.println("Introduce la descripción del producto:");
-        String descripcionProducto = sc.nextLine();
+            System.out.println("Introduce la categoría del producto:");
+            String categoria = sc.nextLine();
 
-        System.out.println("Introduce la categoría del producto:");
-        String categoria = sc.nextLine();
+            ClaseCodigoProducto ccp = new ClaseCodigoProducto();
+            String codigo = ccp.GeneraCodigo(descripcionProducto, categoria);
+            ccp.productos.add(codigo);
 
-        ClaseCodigoProducto ccp = new ClaseCodigoProducto();
-        String codigo = ccp.GeneraCodigo(descripcionProducto, categoria);
-        ccp.productos.add(codigo);
+            System.out.println("El código del producto es: " + codigo);
 
-        System.out.println("El código del producto es: " + codigo);
+        } while (true);
+
 
 
     }
