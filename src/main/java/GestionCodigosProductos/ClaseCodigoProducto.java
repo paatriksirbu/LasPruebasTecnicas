8a488563c4342b37e789da893ed541b5eb92fa82
Patrik <psirb@myuax.com>
2024-04-08 20:55:22 +0200

    El metodo GeneraCodigo devuelve una cadena vacia si recibe un argumento incorrecto.

--- src/main/java/GestionCodigosProductos/ClaseCodigoProducto.java
+++ src/main/java/GestionCodigosProductos/ClaseCodigoProducto.java
@@ -9,14 +9,19 @@ public class ClaseCodigoProducto {
     public static int contadorArticulos = 1; //Inicializamos el contador de articulos a 1
 
     public String GeneraCodigo(String descripcionProducto, String categoria){
-        if (descripcionProducto == null || descripcionProducto.isEmpty() || categoria == null || categoria.isEmpty()){
-            throw new IllegalArgumentException("La descripción del producto y la categoría no pueden estar vacías");
+        try{
+            if (descripcionProducto == null || descripcionProducto.isEmpty() || categoria == null || categoria.isEmpty()){
+                throw new IllegalArgumentException("La descripción del producto y la categoría no pueden estar vacías");
+            }
+
+            String codigoArticulo = String.format("%03d", contadorArticulos);
+            String codigo = descripcionProducto.trim().substring(0,2).toUpperCase() + "-" + categoria.trim().substring(0,2).toUpperCase() + "-" + codigoArticulo;
+            contadorArticulos++;
+
+            return codigo;
+        } catch (IllegalArgumentException e){
+            return "";
         }
 
-        String codigoArticulo = String.format("%03d", contadorArticulos);
-        String codigo = descripcionProducto.trim().substring(0,2).toUpperCase() + "-" + categoria.trim().substring(0,2).toUpperCase() + "-" + codigoArticulo;
-        contadorArticulos++;
-
-        return codigo;
     }
 }
